19c703f3f8c188aa2e34b70be7fe170dbafd6179
calljef <jeferson.nascimento@yahoo.com.br>
2021-01-14 15:30:27 -0300

    Criando pagina de listagem dos registros

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -6,21 +6,20 @@ import Records from './pages/Records';
 import Charts from './pages/Charts';
 
 const Routes = () => (
-    <BrowserRouter>
-        <Header />
-        <Switch>
-            <Route path="/" exact>
-                <Home />
-            </Route>
-            <Route path="/records">
-                <Records />
-            </Route>
-            <Route path="/charts">
-                <Charts />
-            </Route>
-        </Switch>
-    
-    </BrowserRouter>
+  <BrowserRouter>
+    <Header />
+    <Switch>
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      <Route path="/records">
+        <Records />
+      </Route>
+      <Route path="/charts">
+        <Charts />
+      </Route>
+    </Switch>
+  </BrowserRouter>
 );
 
 export default Routes;
